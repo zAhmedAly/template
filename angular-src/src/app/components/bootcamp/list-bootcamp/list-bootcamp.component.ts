de33108bb0fc781713678b5568b5df202b594dd3
Ahmed Aly <aaoflaca@gmail.com>
2019-12-31 02:21:40 -0800

    adding bootcamp map

--- angular-src/src/app/components/bootcamp/list-bootcamp/list-bootcamp.component.ts
+++ angular-src/src/app/components/bootcamp/list-bootcamp/list-bootcamp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-list-bootcamp',
@@ -6,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-bootcamp.component.css']
 })
 export class ListBootcampComponent implements OnInit {
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lng = -71.104028;
+  lat = 42.350846;
+
   constructor() {}
 
   ngOnInit() {
     localStorage.setItem('returnUrl', '/list-bootcamp/000');
+    let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    let map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 7,
+      center: [-71.104028, 42.350846]
+      // interactive: false
+    });
+
+    map.on('load', function() {
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [-71.104028, 42.350846]
+                },
+                properties: {
+                  title: 'DevWorks',
+                  icon: 'college'
+                }
+              },
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [-122.414, 37.776]
+                },
+                properties: {
+                  title: 'Mapbox SF',
+                  icon: 'harbor'
+                }
+              }
+            ]
+          }
+        },
+        layout: {
+          'icon-image': '{icon}-15',
+          'text-field': '{title}',
+          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+          'text-offset': [0, 0.6],
+          'text-anchor': 'top'
+        }
+      });
+    });
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
   }
 }
--- angular-src/src/environments/environment.ts
+++ angular-src/src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken:
+      // 'pk.eyJ1IjoiYWFvZmxhY2EiLCJhIjoiY2s0dG5vODllNHh1bTNsbzZzM2t6dTB3ayJ9.aSw-7Ds5RVBCumtRdqnt8g'
+      'pk.eyJ1IjoiYWFvZmxhY2EiLCJhIjoiY2s0dHAzbHBvMXk3cDNrcGV1NTU0ZnR2cCJ9.ownYyhYrhstEPPHyinrKQw'
+  }
 };
